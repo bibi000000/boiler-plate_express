e7aef20ae66cc27f56d18b953acf2d7ac037a8c9
y <whyyouareay@gmail.com>
2022-11-07 17:38:25 +0900

    [feat] add dotenv

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
+
+app.set('port', process.env.PROT || 3002);
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -22,12 +26,6 @@ app.get('/api/users/user', (req, res) => {
   const user = users.filter(data => data.id==user_id)
   res.json({ok:false, user: user})
 })
-//param
-app.get(`/api/users/{user_id}`, (req, res) => {
-  const user_id = req.params.user_id
-  const user = users.filter(data => data.id==user_id)
-  res.json({ok:false, user: user})
-})
 //body
 app.get(`/api/users/userBody`, (req, res) => {
   const user_id = req.body.user_id
@@ -35,5 +33,11 @@ app.get(`/api/users/userBody`, (req, res) => {
   res.json({ok:false, user:user})
   
 })
+//param
+app.get(`/api/users/:user_id`, (req, res) => {
+  const user_id = req.params.user_id
+  const user = users.filter(data => data.id==user_id)
+  res.json({ok:true, user: user})
+})
 
-app.listen(3000, () => console.log('율2ㅎㅎㅎㅎㅎㅎㅎ'))
+app.listen(app.get('port'), () => console.log('율2ㅎㅎㅎㅎㅎㅎㅎ'))
